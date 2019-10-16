082ff68c1388e8296b494b30a67c42c948638fa1
MLCampbell <56059677+MLCampbell@users.noreply.github.com>
2019-10-16 18:31:39 +1300

    Add files via upload

---
+++ review/includes/include-1571202685646.js
@@ -0,0 +1,5 @@
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/jim/javascript/jim-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scenarios/function-jim-links1571202685646.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/jim/javascript/mobile/custom/jim-custom-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scroll-1571202685646.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/prototype-1571202685646.js'></script>");
